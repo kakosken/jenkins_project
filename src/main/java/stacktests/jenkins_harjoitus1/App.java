1dcb958aacab27bb1f309d7efddf956facebcfee
Koskenniemi <koskenniemi@gmail.com>
2016-12-07 14:27:03 +0200

    first commit

---
+++ src/main/java/stacktests/jenkins_harjoitus1/App.java
@@ -0,0 +1,13 @@
+package stacktests.jenkins_harjoitus1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
